99e7f783dbbda9b093fdc3a0d025c153a689e0b6
Marianela <mari.ytu@gmail.com>
2015-07-26 06:07:25 -0300

    MultilayerSpectral

--- 0. Plots.R
+++ 0. Plots.R
@@ -1,6 +1,6 @@
 #*******************************FUNCTIONS*******************************#
-Spectral <- function (santarosaNormalized, row) {
-  y <- santarosaNormalized[row,]
+Spectral <- function (dataSet, row) {
+  y <- dataSet[row,]
   y <- y[,3:(ncol(y)-1)]
   
   dataPlot <- data.frame(x=integer(), y=integer(), stringsAsFactors=FALSE)
@@ -9,7 +9,8 @@ Spectral <- function (santarosaNormalized, row) {
     dataPlot[nrow(dataPlot)+1,1] <- i
     dataPlot[nrow(dataPlot),2] <- y[1,i]
   }
-  row <- santarosaNormalized[row,2]
+  
+  row <- dataSet[row,2]
   ggplot(data = dataPlot, aes(x = x, y = y, group = 1)) +
     geom_line(colour = "dodgerblue4", alpha = 0.8, size = 1) +
     xlab(paste("Instance", row)) + ylab("Values") +
@@ -62,6 +63,45 @@ PlotWithEllipse <- function (santarosaNormalized, col) {
     )#end theme
 }
 
+MultilayerSpectral <- function (santarosa, row1, row2) {
+  
+  normalize <- function(x){
+    (x - min(x, na.rm=TRUE))/(max(x,na.rm=TRUE) - min(x, na.rm=TRUE))
+  }
+  subsetSantarosa <- subset(santarosa, select = X350:Rdto..gr.parc.) #Subset without 2 first columns
+  normedSubset <- as.data.frame(lapply(subsetSantarosa, normalize)) #Subset normalized
+  #we copy the two first columns back into the data frame, just to maintain the original information
+  twoColumns <- subset(santarosa, select = REPETICION:N.Parcela..Longitud.de.onda) #First two columns of original Dataset
+  santarosaNormalized <- data.frame(twoColumns, normedSubset) #All dataset Normalized
+  dataSet <- santarosaNormalized
+  
+  y1 <- dataSet[row1,]
+  y1 <- y1[,3:(ncol(y1)-1)]
+  y2 <- dataSet[row2,]
+  y2 <- y2[,3:(ncol(y2)-1)]
+  
+  dataPlot <- data.frame(x=integer(), y1=integer(), y2=integer(), stringsAsFactors=FALSE)
+  count <- 0
+  for (i in 1:ncol(y1)) {
+    dataPlot[nrow(dataPlot)+1,1] <- i
+    dataPlot[nrow(dataPlot),2] <- y1[1,i]
+    dataPlot[nrow(dataPlot),3] <- y2[1,i]
+  }
+  
+  names(dataPlot) <- c("x", "y1", "y2")
+  
+  row1 <- dataSet[row1,2]
+  row2 <- dataSet[row2,2]
+  
+  ggplot(data = dataPlot, aes(x)) +
+    geom_line(aes(y=y2), colour = "darkgreen", alpha = 0.8, size = 1) +
+    geom_line(aes(y=y1), colour = "dodgerblue4", alpha = 0.8, size = 1) +
+    xlab(paste("Instances", row1, "and", row2)) + ylab("Values") +
+    theme(panel.grid.minor = element_blank(), #remove gridlines
+          legend.position = "bottom" #legend at the bottom
+    )#end theme
+}
+
 #*******************************MAIN PROGRAM*******************************#
 #load("SantarosaNormalized.Rda") #If you made the second or fourth step (2. NormalizedDataset)
 load("D:/Dropbox/Marianela Iturriaga/data/SantarosaNormalized.Rda") #Absolute path from my computer
@@ -73,6 +113,7 @@ library(ellipse)
 
 Spectral(santarosa, 701) #Instancia 701
 Spectral(santarosa, 141) #Instancia 141
+MultilayerSpectral(santarosa, 701, 141)
 
 Plot1Col(santarosaNormalized, 50) #X397
 Plot1Col(santarosaNormalized, 700) #X1047
